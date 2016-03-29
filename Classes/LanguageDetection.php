797793f1237402169c8b0506fe8dc9666738f4a3
Frank <frankmoll@gmx.net>
2016-03-29 15:33:53 +0200

    Removed unnecessary condition

--- Classes/LanguageDetection.php
+++ Classes/LanguageDetection.php
@@ -624,9 +624,7 @@ class LanguageDetection extends AbstractPlugin {
         $res = $this->getDB()->exec_SELECTquery('tx_sitslanguagedetection_disabledetection', 'sys_domain', 'domainName=\'' . $this->getDB()->fullQuoteStr($domain, 'sys_domain') . '\' AND hidden=0');
         if($this->getDB()->sql_num_rows($res) == 1) {
             if($row = $this->getDB()->sql_fetch_assoc($res)) {
-                if(isset($row['tx_sitslanguagedetection_disabledetection'])) {
-                    $ret = (bool) $row['tx_sitslanguagedetection_disabledetection'];
-                }
+                $ret = (bool) $row['tx_sitslanguagedetection_disabledetection'];
             }
         }
         $this->getDB()->sql_free_result($res);
